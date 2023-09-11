011260e80e1b5f6bfbd866e8c94b321be9826776
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2023-09-11 10:00:33 +0600

    third-commit

--- khabar.js
+++ khabar.js
@@ -5,4 +5,37 @@ const searchFood = () => {
     console.log(searchText);
     searchField.value = '';
 
+    // get api from meal db
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => searchData(data.meals))
+
+}
+
+// get data from meal bd
+
+
+const searchData = meals => {
+    const constainer = document.getElementById('search-food');
+    console.log(meals);
+    meals.forEach(meal=>{
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="col">
+              <div class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top " alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text"></p>
+                </div>
+              </div>
+            </div>
+        `
+        constainer.appendChild(div);
+    })
+    
 }
